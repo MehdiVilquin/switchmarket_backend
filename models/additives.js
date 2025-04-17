be562538162e5096471ffdf63ab37a3cf13326fd
MehdiVilquin <vilquin.mehdi@gmail.com>
2025-04-17 16:40:48 +0200

    additive_models

--- models/additives.js
+++ models/additives.js
@@ -1,13 +1,33 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+import mongoose from "mongoose";
 
-const AdditiveSchema = new Schema({
-  tag: { type: String, unique: true, required: true },
-  shortName: { type: String, required: true },
-  name: { type: Schema.Types.Mixed },
-  description: String,
-  origin: String,
-  risk: String
-});
+const additiveSchema = new mongoose.Schema(
+  {
+    tag: { type: String, required: true, unique: true },
+    shortName: { type: String },
 
-module.exports = mongoose.model('Additive', AdditiveSchema,'additives');
+    name: {
+      fr: { type: String },
+      uk: { type: String },
+    },
+
+    description: {
+      fr: { type: String },
+      uk: { type: String },
+    },
+
+    origin: { type: String },
+    risk: { type: String },
+    fonction: { type: String },
+    famille: { type: String },
+    exemples_produits: { type: String },
+    dja: { type: String },
+    allergie_possible: { type: String, enum: ["Oui", "Non"] },
+    commentaire: { type: String },
+    note: { type: String },
+  },
+  {
+    collection: "additives",
+  }
+);
+
+export default mongoose.model("Additive", additiveSchema);
