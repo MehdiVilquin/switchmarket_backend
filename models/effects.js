3415ec352814f03daa94d09ec9ada0c1f798a4a2
MehdiVilquin <vilquin.mehdi@gmail.com>
2025-04-17 18:53:24 +0200

    effect models

--- models/effects.js
+++ models/effects.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const EffectSchema = new Schema({
+  id: { type: String, required: true, unique: true },
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
-  score: { type: Number, required: true, min: -10, max: 10 }
+  functions: { type: String, required: true }, //
+  toxicity_score: { type: Number, required: true, min: -10, max: 10 },
+  eco_score: { type: Number, required: true, min: -10, max: 10 },
 });
 
-module.exports = mongoose.model('Effect', EffectSchema,'effects');
-
+module.exports = mongoose.model("Effect", EffectSchema, "effects");
