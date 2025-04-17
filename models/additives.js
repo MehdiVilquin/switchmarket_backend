3bbc6282e62bb8a46ffc8211f8bdeb69083b9a23
MehdiVilquin <vilquin.mehdi@gmail.com>
2025-04-17 17:16:56 +0200

    additive_back

--- models/additives.js
+++ models/additives.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const additiveSchema = new mongoose.Schema(
   {
@@ -30,4 +30,4 @@ const additiveSchema = new mongoose.Schema(
   }
 );
 
-export default mongoose.model("Additive", additiveSchema);
+module.exports = mongoose.model("Additive", additiveSchema);
