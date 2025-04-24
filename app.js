417be79c769b9f9b998265d1592084e001891778
MehdiVilquin <vilquin.mehdi@gmail.com>
2025-04-24 17:31:25 +0200

    deploiement back

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ var app = express();
 
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:3001",
+      "https://switchmarket-frontend.vercel.app",
+    ],
     credentials: true,
   })
 );
